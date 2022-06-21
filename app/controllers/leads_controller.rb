723087d35ffb4a49bf9440a2333f0c242d0f1313
ariticas <ariticas10@gmail.com>
2022-06-20 23:18:46 -0700

    hello

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,10 +1,26 @@
 class LeadsController < ApplicationController
-  def new
-  end
+    def new
+        @lead = Lead.new
+    end
+    
+    def show
+        @lead = Lead.find(params[:id])
+    end
+    
+    def create
+        @lead = Lead.new(lead_params)
+        if @lead.save
+       flash.notice = "Thank you! We will be in touch shortly!"
+             redirect_to contact_path
+           else
+             render 'new'
+    end
 
-  def show
-  end
-  
-  def create
-  end
+        end
+    
+    private
+    
+    def lead_params
+        params.require(:lead).permit(:name,:email,:telephone, :comment)
+        end
 end
